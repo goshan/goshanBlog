17418d211f7f5510c8b935d07738f2d6139a9cb8
goshan <goshan.hanqiu@gmail.com>
2014-01-20 21:30:29 +0800

    modified preview and counter cahce

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -34,8 +34,8 @@ class Blog < ActiveRecord::Base
       sounds[music.id.to_s.to_sym] = music.sound.url
     end
     self.text
-    .gsub(/<gImage id=[0-9]*>/) {|s| "<img class='blog_image' src='#{imgs[s.match(/([0-9]+)/)[0].to_sym]}'/>"}
-    .gsub(/<gMusic id=[0-9]*>/) {|s| "<audio controls><source src='#{sounds[s.match(/([0-9]+)/)[0].to_sym]}'>'Your browser does not support the audio element.'</audio>"}
+    .gsub(/<gImage id=[0-9]*>/) {|s| "<img class='blog_image' src='#{imgs[s.match(/([0-9]+)/)[1].to_sym]}'/>"}
+    .gsub(/<gMusic id=[0-9]*>/) {|s| "<audio controls><source src='#{sounds[s.match(/([0-9]+)/)[1].to_sym]}'>'Your browser does not support the audio element.'</audio>"}
     .html_safe
     
     # %audio{:controls => true}
@@ -44,7 +44,10 @@ class Blog < ActiveRecord::Base
   end
   
   def preview
-    self.text.gsub(/<gImage id=[0-9]*>/, "[图片]").gsub(/<gMusic id=[0-9]*>/, "[音乐]")[0..200]
+    self.text
+    .gsub(/<gImage id=[0-9]*>/, "[图片]")
+    .gsub(/<gMusic id=[0-9]*>/, "[音乐]")
+    .gsub(/<a class='gLink' href='.*' target='blank'>.*<\/a>/){|s| "[链接: #{s.match(/<a class='gLink' href='.*' target='blank'>(.*)<\/a>/)[1]}]"}[0..200]
   end
   
   def read_once
@@ -53,7 +56,7 @@ class Blog < ActiveRecord::Base
   
   private
     def check_count_changed
-      if self.blog_type_id_changed?
+      if self.blog_type_id_changed? && self.blog_type_id_change[0]
         BlogType.decrement_counter(:blogs_count, self.blog_type_id_change[0]) #decrement previously assigned Post's counter_cache
         BlogType.increment_counter(:blogs_count, self.blog_type_id_change[1]) #increment newly assigned Post's counter_cache
       end
