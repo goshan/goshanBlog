2d57c687cdc2023cbaccc3f25a903d2803d0292f
goshan <goshan.hanqiu@gmail.com>
2014-01-20 19:21:52 +0800

    fucking

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,7 +1,7 @@
 class Music < ActiveRecord::Base
   attr_accessible :sound, :blog_id
   
-  has_attached_file :sound, :path => ":rails_path/public/musics/:filename", :url => "/musics/:filename"
+  has_attached_file :sound, :path => ":rails_root/public/musics/:filename", :url => "/musics/:filename"
   
   belongs_to :blog
   
