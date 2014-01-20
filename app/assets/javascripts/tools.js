725f9be5d1ea099724daacbfcbcdc063b0df15fe
goshan <goshan.hanqiu@gmail.com>
2014-01-20 20:53:35 +0800

    added link support

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+function on_add_link(){
+	$("#new_blog #link_add a, #edit_blog #link_add a").click(function(e){
+		var text_area = $(this).parent().parent().children(".text_input");
+		text_area.val(text_area.val()+"<a class='gLink' href='' target='blank'></a>");
+		
+		e.preventDefault();
+	});
+}
